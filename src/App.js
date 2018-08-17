f69d336c84d3eaf309f4c3961037e1918958556f
Safet Berberi <berberi.safet@gmail.com>
2018-08-17 10:27:30 -0400

    adding files changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,155 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chart from './Chart';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bills: [],
+        savings: [],
+      currentPage: 1,
+      billsPerPage: 1,
+        months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    };
+      
+    fetch('https://api.myjson.com/bins/125dgk')
+        .then(response => response.json())
+        .then(data =>  this.setState({"bills":data})).catch(error => console.log(error))
+      
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
+    
+    
+        
+    sortData(){
+        let data = this.state.bills;
+        let arrangedSavings = [];
+        let amountPaid = [];
+        
+        for(let i=data.length-1;i>=0;i--){
+            if(data[i].savings !== null){
+                arrangedSavings.push(data[i].savings)
+                amountPaid.push(data[i].bill)
+            }
+                
+        }
+        this.state.savings = arrangedSavings;
+        this.state.billsPaid = amountPaid;
+        
+    }
+        
+
   render() {
+      this.sortData();
+
+    const { bills, currentPage, billsPerPage } = this.state;
+    // Logic for displaying todos
+    const indexOfLastBill = currentPage * billsPerPage;
+    const indexOfFirstBill = indexOfLastBill - billsPerPage;
+    const currentBills = bills.slice(indexOfFirstBill, indexOfLastBill);
+
+    const renderBills = currentBills.map((bill) => {
+      return [
+          
+          
+        <div className="row">
+    <div className="col-sm-6">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title"><b>Name</b></h4>
+            <p className="card-text">John Doe</p>
+          </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title"><b>Year</b></h4>
+        <p className="card-text">{bill.year}</p>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title"><b>Month</b></h4>
+        <p className="card-text">{this.state.months[bill.month-1]}</p>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title"><b>KWH</b></h4>
+        <p className="card-text"> {bill.kwh}</p>
+      </div>
+    </div>
+  </div>
+    <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title"><b>Amount due</b></h4>
+        <p className="card-text">{bill.bill}</p>
+      </div>
+    </div>
+  </div>
+    <div className="col-sm-6">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title"><b>Savings this month</b></h4>
+            <p className="card-text">{bill.savings}</p>
+          </div>
+    </div>
+  </div>
+
+</div>
+    ]});
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(bills.length / billsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={this.handleClick}
+            className="page-link"
+        >
+          {number}
+        </li>
+      );
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+            
+        <ul>
+            <nav className="navbar navbar-light bg-light">
+  <a className="navbar-brand" href="#"> <h2>Solstice Utility Bill</h2></a>
+</nav>
+          {renderBills}
+            <br></br>
+        </ul>
+        <ul id="page-numbers">
+          {renderPageNumbers}
+        </ul>
+        
+        <Chart billData={this.state.bills} savings={this.state.savings} billsPaid={this.state.billsPaid} />
       </div>
     );
   }
 }
 
-export default App;
+ export default App;
---
+++ src/Chart.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import {Bar,Line, Pie} from 'react-chartjs-2';
+
+class Chart extends Component {
+
+    constructor(props){
+        super(props);
+        this.state= {
+
+    }
+    
+    }
+    render(){
+
+        return(
+            <div className="chart">
+                
+                <div >
+                
+                    <Bar
+                    data={{labels: ['December 2016','January 2017','February 2017 ','March 2017','April 2017'],
+            datasets:[{
+                label:'Savings Over Time',
+                data:this.props.savings,
+                backgroundColor:[
+                    'rgba(255,99,132,0.6)',
+                    'rgba(255,99,132,0.6)',
+                    'rgba(255,99,132,0.6)',
+                    'rgba(255,99,132,0.6)',
+                    'rgba(255,99,132,0.6)'
+                ]
+            }]}}
+                    options={{
+                        maintainAspectRatio: false,
+                        responsive : true
+                        }}
+                    
+                />
+
+                                    <Line
+                    data={{labels: ['December 2016','January 2017','February 2017 ','March 2017','April 2017'],
+            datasets:[{
+                label:'Amount paid over time',
+                data:this.props.billsPaid,
+                backgroundColor:[
+                    'rgba(0, 255, 0, 0.3)'   
+                ]
+            }]}}
+                    options={{
+                        maintainAspectRatio: false,
+                        responsive : true
+                        }}
+                    
+                />
+                
+                </div>
+            </div>
+        )
+    }
+
+
+}
+
+export default Chart;
